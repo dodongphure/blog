85a010f30a07f210e1aa3f57fe7015797cf7888d
dodongphure <dodongphure@yahoo.com.vn>
2017-02-09 22:55:30 +0700

    First commit of my super awesome app.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
-//= require jquery_ujs
 //= require tether
+//= require bootstrap
+//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
